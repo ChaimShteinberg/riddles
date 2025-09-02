ebf49b5b61437deaebf3ebe1e5f6bcd8504e3d96
Chaim Shteinberg <a0533197133@gmail.com>
2025-09-02 16:22:44 +0300

    hendle submit form

--- src/components/FormSection.tsx
+++ src/components/FormSection.tsx
@@ -1,8 +1,18 @@
-function FormSection(props: { label: string; id: string; type: string }) {
+function FormSection(props: {
+  label: string;
+  id: string;
+  type: string;
+  setInput: Function;
+}) {
   return (
     <section>
       <label htmlFor={props.id}>{props.label}</label>
-      <input type={props.type} id={props.id} required />
+      <input
+        type={props.type}
+        id={props.id}
+        required
+        onChange={(e) => props.setInput(e.target.value)}
+      />
     </section>
   );
 }
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,12 +1,29 @@
+import { useState } from "react";
 import FormSection from "../components/FormSection";
 
 function Login() {
+  const [username, setUsername] = useState(null);
+  const [password, setPassword] = useState(null);
   return (
     <main>
       <h1>login</h1>
-      <form>
-        <FormSection label="Username" id="Username" type="text" />
-        <FormSection label="Password" id="Password" type="password" />
+      <form
+        onSubmit={(e) => {
+          e.preventDefault(), console.log(username, password);
+        }}
+      >
+        <FormSection
+          label="Username"
+          id="Username"
+          type="text"
+          setInput={setUsername}
+        />
+        <FormSection
+          label="Password"
+          id="Password"
+          type="password"
+          setInput={setPassword}
+        />
         <button>submit</button>
       </form>
     </main>
