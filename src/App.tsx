ab99fdf3282873f67a9f1c4692f37249961e1eaa
Chaim Shteinberg <a0533197133@gmail.com>
2025-09-02 14:23:25 +0300

    header base

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import "./App.css";
 import Home from "./pages/Home.tsx";
+import Header from "./components/Header.tsx";
 
 function App() {
   return (
     <>
+    <Header/>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <header>
+      <h1>
+        <img src="src/assets/icon.riddles.png" alt="logo riddles" /> Riddles
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
