a07090df303fbe41e677a29efb708c49f684e761
Chaim Shteinberg <a0533197133@gmail.com>
2025-09-02 17:41:37 +0300

    riddle base

--- src/pages/Play.tsx
+++ src/pages/Play.tsx
@@ -1,11 +1,13 @@
 import Level from "../components/Level.tsx";
+import Riddle from "../components/Riddle.tsx";
 
 function Play() {
+    
   return (
     <main>
       <h1>Play</h1>
       <Level/>
-      
+      <Riddle/>
     </main>
   );
 }
