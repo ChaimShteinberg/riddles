60a0760b236fbeef752e05400f78e2272bf0feb2
Chaim Shteinberg <a0533197133@gmail.com>
2025-09-02 15:02:42 +0300

    base home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,15 @@
 function Home() {
-  return <h1>Home</h1>;
+  return (
+    <main>
+      <h1>Riddle Game</h1>
+      <p>Challenge Your Mind with Epic Riddles</p>
+      <nav>
+        <button>Play</button>
+        <button>Login</button>
+        <button>Register</button>
+      </nav>
+    </main>
+  );
 }
 
 export default Home;
