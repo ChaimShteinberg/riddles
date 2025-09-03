05231742a8047c94b608d42e9cc062c73814a2bf
Chaim Shteinberg <a0533197133@gmail.com>
2025-09-03 10:05:28 +0300

    header link to home

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,13 @@
-import "../styles/Header.css"
+import { Link } from "react-router";
+import "../styles/Header.css";
 
 function Header() {
   return (
-    <header>
-      <img src="src/assets/icon.riddles.png" alt="logo riddles" />
-      <h1>Riddles</h1>
+    <header id="mainHeader">
+      <Link to="/">
+        <img src="src/assets/icon.riddles.png" alt="logo riddles" />
+        <h1> Riddles</h1>
+      </Link>
     </header>
   );
 }
